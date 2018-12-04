6500c6523fb2eb7ce3f764cada674b3ae4207796
Juan Camilo Morales Niño <juankmilo65@hotmail.com>
2018-12-04 17:45:41 +0300

    WCF Reorganized

---
+++ WcfService1/DataContracts/ResponseSuccessDataContract.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfServiceHotel.DataContracts
+{
+    public class ResponseSuccessDataContract
+    {
+    }
+}
---
+++ WcfService1/Dtos/ErrorDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceHotel.Dtos
+{
+    public class ErrorDto
+    {
+        string errorValue = string.Empty;
+        string typeValue = string.Empty;
+        string codeValue = string.Empty;
+
+        public string Error
+        {
+            get { return errorValue; }
+            set { errorValue = value; }
+        }
+
+        public string Type
+        {
+            get { return typeValue; }
+            set { typeValue = value; }
+        }
+
+        public string Code
+        {
+            get { return codeValue; }
+            set { codeValue = value; }
+        }
+    }
+}
