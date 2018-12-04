b1328c5763cf3eb5b7c9d321974f2c570d072e93
Juan Camilo Morales Niño <juankmilo65@hotmail.com>
2018-12-04 18:16:54 +0300

    Solved compilation

--- WcfServiceHotel/Properties/AssemblyInfo.cs
+++ WcfServiceHotel/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WcfService1")]
+[assembly: AssemblyTitle("WcfServiceHotel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WcfService1")]
+[assembly: AssemblyProduct("WcfServiceHotel")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
