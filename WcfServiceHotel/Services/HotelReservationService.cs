3dccd98b1c4991895c8c9301b0313af16e135f95
Juan Camilo Morales Niño <juankmilo65@hotmail.com>
2018-12-04 17:10:25 +0300

    Base line WCF

---
+++ WcfServiceHotel/Services/HotelReservationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceHotel.Interfaces;
+
+namespace WcfServiceHotel.Services
+{
+    public class HotelReservationService : IHotelReservationInterface
+    {
+        public string GetXMLInformation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
